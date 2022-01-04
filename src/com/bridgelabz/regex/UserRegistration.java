33711e6440e52dd9a6bfd6ce97a7bd27bae40f27
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-04 22:43:05 +0530

    UPDATED: validation of password have at least one special character

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration {
 		private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}$";
 		private static final String EMAIL_REGEX = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.a-z]+$";
 		private static final String PHONE_NUMBER_REGEX = "^[0-9]{2}[\\s][0-9]{10}$";
-		private static final String PASSWORD_REGEX = "(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";
+		private static final String PASSWORD_REGEX = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9].{8,}$";
 
 		public void register(UserDetails userDetail) {
 			
@@ -16,7 +16,7 @@ public class UserRegistration {
 			boolean isPhoneNumberValid = validateUserDetails(PHONE_NUMBER_REGEX,userDetail.getPhoneNumber());
 			boolean isPasswordValid = validateUserDetails(PASSWORD_REGEX,userDetail.getPassword());
 
-			if(isFirstNameValid && isLastNameValid && isEmailValid  && isPasswordValid) {
+			if(isFirstNameValid && isLastNameValid && isEmailValid  && isPhoneNumberValid && isPasswordValid) {
 				System.out.println("Details entered are valid");
 			}
 			else {
